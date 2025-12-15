b0d9ae3034d6ef97bffb713e84562c25c25aef31
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-12-15 15:42:09 +1100

    fix(populatedb): properly escape single quotation marks. increase length of description of coffee beans
    escaped using double single-quotation marks like this: https://stackoverflow.com/a/38701038 . increased length of the description - 280 characters was certainly not enough and it's just better to just future proof this i think!

--- src/db/populatedb.js
+++ src/db/populatedb.js
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS items (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100) NOT NULL,
     size_grams SMALLINT NOT NULL,
-    roastery VARCHAR ( 100 ) NOT NULL,
-    description VARCHAR ( 280 ),
+    roastery VARCHAR(100) NOT NULL,
+    description VARCHAR(4000),
     price_cents SMALLINT NOT NULL,
     stock_quantity NUMERIC NOT NULL,
     category_id INTEGER REFERENCES categories(id)
@@ -36,9 +36,9 @@ VALUES
     ('Ethiopian Yirgacheffe', 1000, 'The Coffee Company', 'Known around the world as one of the best coffees, prized by connoisseurs for its winey cupping characteristics. This is a great treat in a plunger or through a filter.', 4600, 461, 1),
     ('Sacred Grounds Breeze Blend', 200, 'Sacred Grounds', 'Sacred Grounds Breezy Blend Ground Coffee specialty coffee. It is a dark roast ground coffee, freshly roasted. Dark chocolate, roasted almond. Do ya thing!', 1700, 172, 2),
     ('Campos Decaf Ground Coffee', 250, 'Campos', 'Making great coffee at home has never been easier! Our Campos Decaf coffee, ground in a resealable 250g bag is ready for you to brew at home. Decaf coffee is for coffee lovers. For people who enjoy the flavour and experience of coffee without the caffeine. We have always taken great pride in our decaf for exactly this reason.', 2200, 221, 2),
-    ('Campos Superior Aluminium Coffee Capsules 10 pack', 55, 'Campos', 'If you have ordered a cup of Campos coffee in our famous green cup, then you've tasted our Campos Superior Blend. Campos Superior is our signature, quintessential specialty coffee cafe blend. It's what we've proudly served in our cafes since day in Newtown, 2002.', 800, 81, 3),
+    ('Campos Superior Aluminium Coffee Capsules 10 pack', 55, 'Campos', 'If you have ordered a cup of Campos coffee in our famous green cup, then you''ve tasted our Campos Superior Blend. Campos Superior is our signature, quintessential specialty coffee cafe blend. It''s what we''ve proudly served in our cafes since day in Newtown, 2002.', 800, 81, 3),
     ('Vittoria Nespresso Compatible Coffee Capsules 10 pack', 52, 'Vittoria', 'Vittoria Italian blend is a medium roast of 100% Arabica coffee beans from Central and South America with a balanced body and taste. Italian blend is a milder cup compared to darker roasts like Vittoria Espresso.', 475, 48, 3),
-    ('Some uncategorised coffee', 250, 'Roastery X', 'Lorem ipsum blah blah', 2500, 15);
+    ('Some uncategorised coffee', 250, 'Roastery X', 'Lorem ipsum blah blah', 2500, 15, NULL);
 `;
 
 async function main() {
@@ -51,7 +51,7 @@ async function main() {
 
 	await client.connect();
 	await client.query(CREATE_TABLES_QUERY);
-	//   await client.query(POPULATE_TABLES_QUERY);
+	await client.query(POPULATE_TABLES_QUERY);
 	await client.end();
 	console.log("done");
 }
