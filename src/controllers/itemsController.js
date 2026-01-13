d9e62e70a10f3e0ea259612b2545d1daa05eb4ae
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-13 16:37:51 +1100

    feat(errors): use a custom error for when an item is not found by its ID

--- src/controllers/itemsController.js
+++ src/controllers/itemsController.js
@@ -1,4 +1,5 @@
 const db = require("../db/queries");
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
 
 const getAllItems = async (_req, res) => {
 	const allItems = await db.getAllItems();
@@ -9,7 +10,8 @@ const getItemById = async (req, res) => {
 	const { id: itemId } = req.params;
 	const item = await db.getItemById(itemId);
 
-	if (item === null) throw new Error(`Item with ID ${itemId} not found`);
+	if (item === null)
+		throw new CustomNotFoundError(`Item with ID ${itemId} not found`);
 
 	res.render("pages/items/itemDetailsPage", { item });
 };
