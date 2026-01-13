5821f04683c1af3b2ae1046688abdba3737788e0
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-13 11:23:34 +1100

    feat(populatedb): when creating 'categories' table, constraint is placed on the name of the category such that it is unique
    this fixes #5

--- src/db/populatedb.js
+++ src/db/populatedb.js
@@ -6,7 +6,7 @@ const { DATABASE_NAME } = require("./constants");
 const CREATE_TABLES_QUERY = `
 CREATE TABLE IF NOT EXISTS categories (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) UNIQUE NOT NULL,
     description VARCHAR(280)
 );
 
@@ -35,7 +35,7 @@ VALUES
     ('Ethiopian Yirgacheffe', 'ETH-YIRG-500G', 500, 'The Coffee Company', 'Known around the world as one of the best coffees, prized by connoisseurs for its winey cupping characteristics. This is a great treat in a plunger or through a filter.', 2300, 235, 1),
     ('Ethiopian Yirgacheffe', 'ETH-YIRG-1KG', 1000, 'The Coffee Company', 'Known around the world as one of the best coffees, prized by connoisseurs for its winey cupping characteristics. This is a great treat in a plunger or through a filter.', 4600, 461, 1),
     ('Sacred Grounds Breeze Blend', 'SG-BREEZE-200G', 200, 'Sacred Grounds', 'Sacred Grounds Breezy Blend Ground Coffee specialty coffee. It is a dark roast ground coffee, freshly roasted. Dark chocolate, roasted almond. Do ya thing!', 1700, 172, 2),
-    ('Campos Superior Aluminium Coffee Capsules 10 pack', 'CAM-SUP-10PK', 55, 'Campos', 'If you have ordered a cup of Campos coffee in our famous green cup, then you''ve tasted our Campos Superior Blend. Campos Superior is our signature, quintessential specialty coffee cafe blend. It''s what we''ve proudly served in our cafes since day in Newtown, 2002.', 800, 81, 3),
+    ('Campos Superior Aluminium Coffee Capsules 10 pack', 'CAM-SUP-10PK', 55, 'Campos', 'If you have ordered a cup of Campos coffee in our famous green cup, then you''ve tasted our Campos Superior Blend. Campos Superior is our signature, quintessential specialty coffee cafe blend. It''s what we''ve proudly served in our cafes since day in Newtown, 2002.', 800, 81, 3);
 `;
 
 async function main() {
