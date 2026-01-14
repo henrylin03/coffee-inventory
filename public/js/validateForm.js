347e9fe703464546f692152d49012825e296f3a6
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-14 16:30:10 +1100

    feat(editItemPage): onSubmit of form, inputs are validated. if input is not valid, then the form cannot be submitted, and a .error class is added to that input's parent container
    add .error class to parent container of input so that the label can also be styled

--- public/js/validateForm.js
+++ public/js/validateForm.js
@@ -1,1 +1,23 @@
-console.log("hello");
+const form = document.querySelector("form");
+const inputListItems = form.querySelectorAll("li");
+const saveButton = document.querySelector("input[type='submit']");
+
+form.noValidate = true; // disable default html validation
+
+function validateInput(inputListItem) {
+	const inputElement = inputListItem.querySelector("input, textarea");
+
+	if (inputElement.checkValidity()) inputListItem.classList.remove("error");
+	else inputListItem.classList.add("error");
+}
+
+function validateForm(event) {
+	if (form.checkValidity()) form.submit();
+
+	event.preventDefault();
+	inputListItems.forEach((inputListItem) => {
+		validateInput(inputListItem);
+	});
+}
+
+saveButton.addEventListener("click", validateForm);
