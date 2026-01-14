ccf7849595aa4789b40a3c028d1cd5bc37605d00
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-15 10:24:01 +1100

    feat(validateItem): add validation for 'name' of item, and its 'sku'
    to make it easier to configure and maintain in future, i have a hashmap that maps each field to its min and max characters. these align with the postgres populatedb.js limitations on the fields. for sku, i've also added a alphanumeric validation. this aligns with best practices in inventory management where sku doesn't contain spaces, but has hyphens and underscores instead to separate. i know this isn't something i have in the client-side validation yet, but i will add this in future.

--- src/controllers/validateItem.js
+++ src/controllers/validateItem.js
@@ -1,5 +1,44 @@
 const { body } = require("express-validator");
 
-const validateItem = [];
+const CHARACTER_LIMITS = {
+	name: { min: 1, max: 100 },
+	sku: { min: 1, max: 24 },
+	description: { min: 0, max: 4000 },
+};
+
+const getLengthErrorMessage = (fieldName, minCharacters, maxCharacters) =>
+	`${fieldName} must be between ${minCharacters} and ${maxCharacters} characters`;
+
+const alphanumericErrorMessage =
+	"must only contain letters, numbers, hyphens (-) or underscores (_)";
+
+const validateItem = [
+	body("name")
+		.trim()
+		.isLength({
+			min: CHARACTER_LIMITS.name.min,
+			max: CHARACTER_LIMITS.name.max,
+		})
+		.withMessage(
+			getLengthErrorMessage(
+				"Name",
+				CHARACTER_LIMITS.name.min,
+				CHARACTER_LIMITS.name.max,
+			),
+		),
+
+	body("sku")
+		.trim()
+		.isAlphanumeric("en-AU", { ignore: ["-", "_"] })
+		.withMessage(`SKU ${alphanumericErrorMessage}`)
+		.isLength({ min: CHARACTER_LIMITS.sku.min, max: CHARACTER_LIMITS.sku.max })
+		.withMessage(
+			getLengthErrorMessage(
+				"SKU",
+				CHARACTER_LIMITS.sku.min,
+				CHARACTER_LIMITS.sku.max,
+			),
+		),
+];
 
 module.exports = { validateItem };
