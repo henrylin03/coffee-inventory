a673e7b61695bc0d3dd180530351dc03e39143b1
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-15 17:03:08 +1100

    feat(itemsTable): selling price is formatted according to Australian (AUD) standards with formatCurrency helper
    opted to pass in cents because the 'raw' data in database is in cents, so we'd likely be passing this in. i have not added a space after the dollar sign because it isn't here by default for Australia, so I don't want to be going against what is the standard. I may add the space if legibility becomes an issue later on, if i change my mind...

---
+++ src/helpers/formatHelpers.js
@@ -0,0 +1,6 @@
+const formatCurrency = (moneyInCents) =>
+	new Intl.NumberFormat("en-AU", { style: "currency", currency: "AUD" }).format(
+		Number(moneyInCents) / 100,
+	);
+
+module.exports = { formatCurrency };
