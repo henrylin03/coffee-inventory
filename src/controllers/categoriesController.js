c0c7c8cc3f5d94089eb718fd10875fa2b5d19145
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-22 10:01:10 +1100

    feat(category): when user goes to /category/new, router calls controller's .createCategoryGet method, which shows user the newCategory.ejs view (just placeholder for now)

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -4,3 +4,7 @@ exports.getAllCategories = async (_req, res) => {
 	const allCategories = await db.getAllCategories();
 	res.render("pages/allCategories", { categories: allCategories });
 };
+
+exports.createCategoryGet = async (_req, res) => {
+	res.render("pages/newCategory");
+};
--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -4,5 +4,6 @@ const categoriesController = require("../controllers/categoriesController");
 const categoriesRouter = Router();
 
 categoriesRouter.get("/", categoriesController.getAllCategories);
+categoriesRouter.get("/new", categoriesController.createCategoryGet);
 
 module.exports = categoriesRouter;
