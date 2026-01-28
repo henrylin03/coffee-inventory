4d8c50daf16de6c5c58dacf0efd056576ae9c7b8
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-29 09:57:03 +1100

    refactor(navDrawer): navLink is its own separate subcomponent

--- public/js/navScript.js
+++ public/js/navScript.js
@@ -1,4 +1,4 @@
-const navlinks = document.querySelectorAll("nav .navlink");
+const navlinks = document.querySelectorAll(".navlink");
 
 function styleActiveLink() {
 	const ACTIVE_LINK_CLASS = "menu-active";
@@ -6,6 +6,7 @@ function styleActiveLink() {
 
 	[...navlinks].forEach((elem) => {
 		const linkPath = elem.getAttribute("href").split("/")[1];
+		console.log(linkPath);
 
 		if (linkPath === pathOfCurrentPage) elem.classList.add(ACTIVE_LINK_CLASS);
 		else elem.classList.remove(ACTIVE_LINK_CLASS);
