dc2a870a7c746ade6c142fdbab4c49cb1ed5eb24
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-30 21:54:53 +1100

    fix(compositeItemsScript): clicking on checkbox does not cause user to be routed to that item's 'edit item' page
    stopPropagation only works on same event types, so i had to change the checkbox event to click rather than change (because its parent row event is also click

--- public/js/compositeItemsScript.js
+++ public/js/compositeItemsScript.js
@@ -4,20 +4,18 @@ const rows = document.querySelectorAll("tbody tr");
 
 // TODO: if there is an existing item already part of category, its checkbox is checked
 
-const handleChangeOnSelectAllCheckbox = () => {
-	for (const checkbox of rowCheckboxes) checkbox.checked = !checkbox.checked;
-};
-
-const handleChangeOnRowCheckbox = (e) => {
-	e.stopPropagation();
-};
-
 const handleClickOnRow = (e) => {
 	const itemId = e.currentTarget.dataset.itemId;
 	location.href = `/items/${itemId}`;
 };
 
-// TODO: prevent event bubbling situation with the row
+const handleChangeOnSelectAllCheckbox = () => {
+	for (const checkbox of rowCheckboxes) checkbox.checked = !checkbox.checked;
+};
+
+const handleClickOnRowCheckbox = (e) => {
+	e.stopPropagation();
+};
 
 // TODO: if only some of checkboxes in row is selected, then select all checkbox is 'indeterminate'
 
@@ -25,6 +23,6 @@ const handleClickOnRow = (e) => {
 selectAllCheckbox.addEventListener("change", handleChangeOnSelectAllCheckbox);
 
 for (const checkbox of rowCheckboxes)
-	checkbox.addEventListener("change", handleChangeOnRowCheckbox);
+	checkbox.addEventListener("click", handleClickOnRowCheckbox);
 
 for (const row of rows) row.addEventListener("click", handleClickOnRow);
