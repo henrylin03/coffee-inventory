9000918b8e506ce839d092495d6b3e443dcd6516
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-30 21:50:22 +1100

    refactor(compositeItemsScript): clicking on item row is no handled by script
    just makes it easier to know that we're doing everything client-side within the script

--- public/js/compositeItemsScript.js
+++ public/js/compositeItemsScript.js
@@ -1,5 +1,6 @@
 const selectAllCheckbox = document.querySelector("thead .checkbox");
 const rowCheckboxes = document.querySelectorAll("tbody .checkbox");
+const rows = document.querySelectorAll("tbody tr");
 
 // TODO: if there is an existing item already part of category, its checkbox is checked
 
@@ -7,7 +8,14 @@ const handleChangeOnSelectAllCheckbox = () => {
 	for (const checkbox of rowCheckboxes) checkbox.checked = !checkbox.checked;
 };
 
-const handleChangeOnRowCheckbox = () => {};
+const handleChangeOnRowCheckbox = (e) => {
+	e.stopPropagation();
+};
+
+const handleClickOnRow = (e) => {
+	const itemId = e.currentTarget.dataset.itemId;
+	location.href = `/items/${itemId}`;
+};
 
 // TODO: prevent event bubbling situation with the row
 
@@ -18,3 +26,5 @@ selectAllCheckbox.addEventListener("change", handleChangeOnSelectAllCheckbox);
 
 for (const checkbox of rowCheckboxes)
 	checkbox.addEventListener("change", handleChangeOnRowCheckbox);
+
+for (const row of rows) row.addEventListener("click", handleClickOnRow);
