3215ad376bc7663ad4ea40adb1561057cc8dedf3
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-30 21:41:20 +1100

    feat(editItemsInCategory): unchecking the 'select all' checkbox (when it's fully selected), unchecks/unselects everything in table

--- public/js/compositeItemsScript.js
+++ public/js/compositeItemsScript.js
@@ -3,9 +3,8 @@ const rowCheckboxes = document.querySelectorAll("tbody .checkbox");
 
 // TODO: if there is an existing item already part of category, its checkbox is checked
 
-// TODO: if user checks the 'select all' checkbox, all checkboxes are selected
 const handleClickOnSelectAllCheckbox = () => {
-	for (const checkbox of rowCheckboxes) checkbox.checked = true;
+	for (const checkbox of rowCheckboxes) checkbox.checked = !checkbox.checked;
 };
 
 // TODO: prevent event bubbling situation with the row
