d63891118250334e73f6a93103d5d67afa36a612
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-04 09:56:05 +1100

    feat(editItem.ejs): set the cancel button's href attribute such that if there is a 'referredCategoryId' optional query parameter, then we take users back to that category's details page. if not, then we take it back to the 'all items' page

--- public/js/editItemScript.js
+++ public/js/editItemScript.js
@@ -5,6 +5,23 @@ function handleKeydownOnNumberInputs(event) {
 	}
 }
 
+function setCancelBtnHref() {
+	const QUERY_KEY = "referredCategoryId";
+	const cancelBtn = document.querySelector(".cancel-btn");
+
+	//   console.log("window.location.search:", location.search);
+	const params = new URL(document.location.toString()).searchParams;
+	const referredCategoryId = params.get(QUERY_KEY);
+
+	if (referredCategoryId === null) {
+		cancelBtn.setAttribute("href", "/items");
+	} else {
+		cancelBtn.setAttribute("href", `/categories/${referredCategoryId}`);
+	}
+	// TODO: NEED TO ALSO HANDLE SITUATION WHERE USER IS GOING TO EDIT ITEMS FROM "MANAGE ITEMS"
+}
+
+setCancelBtnHref();
 /* attach event listeners */
 const form = document.querySelector("form");
 const numberInputs = form.querySelectorAll('input[type="number"]');
